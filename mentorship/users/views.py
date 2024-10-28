c78b0cda9a4f04505cd01aa0f0d3a8f2e13cf9c8
s.smirnov <s.smirnov@tm-industries.ru>
2024-10-28 12:11:08 +0300

    Add serializers and views

--- mentorship/users/views.py
+++ mentorship/users/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import UserRegistationSerializer, UserSerializer
+from rest_framework.views import APIView
 
-# Create your views here.
+
+User = get_user_model()
+
+
+class UserRegistationView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegistationSerializer
+
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class UserDetailView(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        obj = super().get_object()
+
+        if self.request.user != obj:
+            self.permission_denied(self.request, message="You can only edit your own data.")
+        
+        return obj
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
